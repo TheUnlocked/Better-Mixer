6191af2b35581bf955b96f9bfadc646c7049f739
Unlocked <10186337+TheUnlocked@users.noreply.github.com>
2019-12-19 17:54:41 -0800

    Update content.js

--- Extension/content.js
+++ Extension/content.js
@@ -7,6 +7,65 @@ if (DEBUG_MODE) {
     console.log("%cBetterMixer.instance.postToContent({message: 'clearAllConfigs'})", hintFormatting);
 }
 
+if (window.location.search) {
+    const sp = new URLSearchParams(window.location.search);
+    if (sp.get('bettermixer-message')) {
+        (async () => {
+            const currentUser = await (await fetch('https://mixer.com/api/v1/users/current')).json();
+            const validateAuth = async auth => {
+                try {
+                    const response = await fetch('https://mixer.com/api/v1/oauth/token/introspect', {
+                        method: "POST",
+                        headers: {
+                            'content-type': 'application/json'
+                        },
+                        body: JSON.stringify({token: auth})
+                    });
+                    const data = await response.json();
+                    if (data.active &&
+                        data.scope.split(' ').includes('channel:update:self') &&
+                        data.username === currentUser.username &&
+                        data.client_id === '473772fa4fa6d46b0c73f2aca560215cf602d1c2e925e521') {
+                        return true;
+                    }
+                }
+                catch { }
+                return false;
+            };
+            const getJWT = async () => {
+                const response = await fetch('https://mixer.com/api/v1/jwt/authorize', {
+                    method: "POST",
+                    headers: {'client-id': '473772fa4fa6d46b0c73f2aca560215cf602d1c2e925e521'}
+                });
+                const jwt = response.headers.get('x-jwt');
+                return jwt;
+            };
+            switch (sp.get('bettermixer-message')) {
+                case 'description':
+                    if (await validateAuth(sp.get('bettermixer-auth'))) {
+                        if (sp.get('bettermixer-message-data')) {
+                            const jwt = await getJWT();
+                            const patchResponse = await fetch(`https://mixer.com/api/v1/channels/${currentUser.channel.id}`, {
+                                method: "PATCH",
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                    'authorization': `JWT ${jwt}`
+                                },
+                                body: JSON.stringify({"description": atob(sp.get('bettermixer-message-data').replace(/ /g, '+'))})
+                            });
+                            if (patchResponse.ok) {
+                                window.close();
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    console.log(`Unknown Better Mixer message ${sp.get('bettermixer-message')}`);
+            }
+        })();
+    }
+}
+
 const onLoad = () => {
     const injection = document.createElement('script');
     injection.src = chrome.runtime.getURL('content/BetterMixer.js');
